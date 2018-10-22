580d9556ef1d770fb8a3ed0a084f354f491c8d45
michalkasiarz <michal.kasiarz@post.com>
2018-10-22 08:27:11 +0200

    logging test on salesforce.com using different locators

---
+++ validating-error-login-message/Main.java
@@ -0,0 +1,31 @@
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.setProperty("webdriver.chrome.driver", "//home//michal//chrome_driver//chromedriver");
+        WebDriver driver = new ChromeDriver();
+
+        /* Logging on salesforce.com
+        using different locators and validating the error login message
+         */
+
+        // hitting salesforce.com login URL
+        driver.get("https://login.salesforce.com/");
+
+        // locating element - email box - using xpath locator
+        driver.findElement(By.xpath("//*[@id=\"username\"]")).sendKeys("test1");
+
+        // locating element - password box - using CSS selector
+        driver.findElement(By.cssSelector("#password")).sendKeys("test1");
+
+        // logging in using id locator
+        driver.findElement(By.id("Login")).click();
+
+
+    }
+}
